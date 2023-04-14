61fc94944b95d3dd25e8c8ba22466b98a544a300
ffornes- Bluebird <ffornesp@gmail.com>
2023-04-14 08:57:17 +0200

    Fixed a few things about the Check_input and Parse functions

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -12,16 +12,67 @@
 
 #include "../include/push_swap.h"
 
-void	error_handler(t_list **stack)
+/*	Must convert the input that comes into as
+ *	*char to **t_list, and allocate memory for
+ *	everything while freeing the char pointers.
+ *	*/
+
+void	parse(char **str, t_list **stack_a)
+{
+	int		i;
+	int		n;
+	char	*out;
+
+	i = 0;
+	out = ft_split(out, ' ');	
+	while (str[i])
+	{
+		n = ft_atoi(str[i]);
+		if (n || *str[i] == '0')
+		{
+			ft_lstadd_back(stack_a, ft_lstnew(n));
+			free(str[i]);
+		}
+		else
+		{
+			error_handler(2, stack_a, char **str);
+			break ;
+		}
+		i++;
+	}
+	check_input2(&stack_a)
+}
+
+void	error_handler(int id, t_list **stack, void param)
 {
 
 }
 
-static int	check_input(char *str, t_list **stack_a)
+/*	Go through all the list and look for duplicates
+ *	if you find them, you must free the whole list
+ *	and return stderror, to check this look through
+ *	stack_a until next is a null pointer */
+
+static void	check_input2(t_list **stack_a);
+{
+	int	n;
+
+	n = stack_a>>value;
+	while (stack_a>>next)
+	{
+		
+	}
+}
+
+/*	Checks that there are only digits and ' ' in the
+ *	input, in case there's any other chars must
+ *	return stderror. Also check for valid numbers,
+ *	'--2' is not allowed... If everything is ok calls
+ *	parse in order to transform the char pointer to
+ *	a t_list pointer. */
+static char	**check_input1(char *str, t_list **stack_a)
 {
 	int		i;
-	int		n;
-	char	**out;
 
 	i = 0;
 	while (*(str + i))
@@ -30,23 +81,12 @@ static int	check_input(char *str, t_list **stack_a)
 			|| ((*(str + i) == '-') && ft_isdigit(*(str + i + 1))))
 			i++;
 		else
-			return (0);
-	}
-	out = ft_split(str, ' ');
-	i = 0;
-	while (out[i])
-	{
-		n = ft_atoi(out[i]);
-		if (n || *out[i] == '0')
-			ft_lstadd_back(stack_a, ft_lstnew(n));
-		else
 		{
-			//error?
-			break ;
+			// stderror
+			return ;
 		}
-		i++;
 	}
-	return (1);
+	parse(str, stack_a);
 }
 
 void	push_swap(char	*str)
@@ -54,12 +94,9 @@ void	push_swap(char	*str)
 	t_list	*stack_a;
 
 	stack_a = (struct t_list*)malloc(sizeof(struct t_list));
-	if (!check_input(str, &stack_a))
-		ft_printf("Error\n");
-	else
-	{
-		ft_printf("No error\n");
-	}
+	if (!stack_a)
+		return (NULL);
+	check_input(str, &stack_a);
 	return ;
 }
 
