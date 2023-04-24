bd828b52379b9b81ed4323802b018b8dd4d6c218
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-04-24 10:00:58 +0200

    Modified function, now only checks if stack_a is in the correct order, it doesn't check if stack_b is empty

--- srcs/check_sort.c
+++ srcs/check_sort.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 17:46:10 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/04/20 17:51:09 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/04/24 09:59:45 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,19 @@ int	finish_check(m_stack *stk)
 	int		*n;
 	t_list	*lst;
 
-	if (!stk->stack_b)
+	lst = stk->stack_a;
+	max = lst->content;
+	min = lst->content;
+	while (lst->next)
 	{
-		lst = stk->stack_a;
-		max = lst->content;
-		min = lst->content;
-		while (lst->next)
+		n = lst->next->content;
+		if (*n > *max && *n > *min)
 		{
-			n = lst->next->content;
-			if (*n > *max && *n > *min)
-			{
-				max = lst->next->content;
-				lst = lst->next;
-			}
-			else
-				return (0);
+			max = lst->next->content;
+			lst = lst->next;
 		}
-		return (1);
+		else
+			return (0);
 	}
-	return (0);
+	return (1);
 }
