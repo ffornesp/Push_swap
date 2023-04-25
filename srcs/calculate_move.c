6544708c92a8d4f239c622f2fee0f5506e0d2ddd
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-04-25 10:46:48 +0200

    All cases done

--- srcs/calculate_move.c
+++ srcs/calculate_move.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 10:06:14 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/04/24 16:43:57 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/04/25 10:24:33 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,30 @@ static void	check_this(t_list *lst)
 	ft_printf("\n");
 }
 
+static int 	*find_p(int n, t_list *lst)
+{
+	t_list	*aux;
+	int		*i;
+	int		*target;
+
+	aux = lst;
+	target = NULL;
+	while (aux)
+	{
+		i = aux->content;
+		if (!target && *i > n)
+			target = i;
+		if (*i > n && *i < *target)
+			target = i;
+		aux = aux->next;
+	}
+	return (target);
+}
+
 t_list	*calculate_moves(m_stack *stk, int *max_b, int *min_b)
 {
 	t_list	*cheapest_action;
-	t_list	*tmp_action;
+	t_list	*tmp_act;
 	t_list	*aux;
 	int		*i;
 
@@ -40,31 +60,37 @@ t_list	*calculate_moves(m_stack *stk, int *max_b, int *min_b)
 	cheapest_action = NULL;
 	while (aux)
 	{
-		tmp_action = ft_lstnew(NULL);
+		tmp_act = ft_lstnew(NULL);
 		i = aux->content;
 		ft_printf(YELLOW"Checking %d\n"WHITE, *i);
 		if (*i > *max_b || *i < *min_b)
 		{
-			calc_rotation(stk->stack_b, max_b, 1, &tmp_action);
-			calc_rotation(stk->stack_a, i, 0, &tmp_action);
-			check_this(tmp_action);
-			check_merge_rotation(&tmp_action);
-			check_this(tmp_action);
+			calc_rot(stk->stack_b, max_b, 1, &tmp_act);
+			calc_rot(stk->stack_a, i, 0, &tmp_act);
+			check_this(tmp_act);
+			check_merge_rotation(&tmp_act);
+			add_push(&tmp_act, 0);
+			check_this(tmp_act);
+
 		}
 		else
 		{
-			// Rotate b until I can push lst between [> lst >]
+			calc_rot(stk->stack_b, find_p(*i, stk->stack_b), 1, &tmp_act);
+			calc_rot(stk->stack_a, i, 0, &tmp_act);
+			check_this(tmp_act);
+			check_merge_rotation(&tmp_act);
+			add_push(&tmp_act, 0);
+			check_this(tmp_act);
 		}
 		if (!cheapest_action)
-			cheapest_action = tmp_action;
-		else if (ft_lstsize(cheapest_action) > ft_lstsize(tmp_action))
+			cheapest_action = tmp_act;
+		else if (ft_lstsize(cheapest_action) > ft_lstsize(tmp_act))
 		{
 			ft_lstclear(&cheapest_action, (void *)ft_delete);
-			cheapest_action = tmp_action;
+			cheapest_action = tmp_act;
 		}
 		aux = aux->next;
 	}
-	//min_max_change_check(stk->stack_b->content, max_b, min_b);
 	ft_printf(GREEN"Found cheapest move: %d moves\n"WHITE, ft_lstsize(cheapest_action));
 	return (cheapest_action);
 }
